9e426cb224d198aa48cf9ede4c07b58014d174ab
Jose Antonio Magaña Mesa <jose_a_magana@hotmail.com>
2014-01-19 12:27:03 +0100

    karate tests

--- src/cimemetics.py
+++ src/cimemetics.py
@@ -33,7 +33,9 @@ dgt3={1: [1, 5, 6, 7, 9, 13, 17, 19, 22, 25, 26, 27, 31, 32, 41, 48, 54, 56, 57,
 
 # TODO's
 # max_intersect>1 would cause that splitted nodes are never added to a community if they only have one edge (leaves)
+# average partial_results across repetitions 
 # quick evolution of the modularity
+# dynamic end criterion finishes less than 1000 (if 500 as change)
 # consider large clusters on improve_offspring
 
 
@@ -43,11 +45,11 @@ MAX_GENERATIONS=0
 Pm=0
 init_iter_factor=0.2  # from the paper
 nreps=20
+dyn=False
 #problem parameters
 problem_name=''
-error=0
-ok=0
 generations=0
+nochange=0
 mutations=0
 creplacements=0
 wreplacements=0
@@ -64,29 +66,52 @@ nedges=0
 #(population_size,MAX_GENERATIONS,Pm,cmin)
 default=(10,200,0.2,'default.txt')
 dejong=(50,1000,0.001,'dejong.txt')             #best option
-dejong11=(50,2000,0.001,'dejong11.txt')         #MAX_GENERATIONS
-dejong12=(50,4000,0.001,'dejong12.txt')
-dejong13=(50,8000,0.001,'dejong13.txt')
-dejong21=(100,1000,0.001,'dejong21.txt')        #population_size
-dejong22=(150,1000,0.001,'dejong22.txt')
-dejong23=(200,1000,0.001,'dejong23.txt')
-dejong24=(300,1000,0.001,'dejong24.txt')
-dejong30=(50,1000,0.0,'dejong30.txt')           #Pm
-dejong31=(50,1000,0.01,'dejong31.txt')   
-dejong32=(50,1000,0.05,'dejong32.txt')
-dejong33=(50,1000,0.1,'dejong33.txt')
-dejong34=(50,1000,0.2,'dejong34.txt')
-dejong35=(50,1000,0.4,'dejong35.txt')
-dejong36=(50,1000,0.6,'dejong36.txt')
-dejong37=(50,1000,0.9,'dejong37.txt')
+
 grefenstette=(30,1000,0.01,'grefenstette.txt')  # all options
+
+grefenstettedyn=(30,1000,0.01,'grefenstettedyn.txt')  # dyn
+
+grefenstette11=(30,2000,0.01,'grefenstette11.txt')        #MAX_GENERATIONS
+grefenstette12=(30,4000,0.01,'grefenstette12.txt')
+grefenstette13=(30,8000,0.01,'grefenstette13.txt')
+grefenstette14=(30,16000,0.01,'grefenstette14.txt')
+grefenstette15=(30,32000,0.01,'grefenstette15.txt')
+grefenstette16=(30,64000,0.01,'grefenstette16.txt')
+grefenstette17=(30,128000,0.01,'grefenstette17.txt')
+
+grefenstette20=(10,1000,0.01,'grefenstette20.txt')        #population_size
+grefenstette21=(20,1000,0.01,'grefenstette21.txt')        
+grefenstette22=(50,1000,0.01,'grefenstette22.txt')        
+grefenstette23=(100,1000,0.01,'grefenstette23.txt')        
+grefenstette24=(150,1000,0.01,'grefenstette24.txt')
+grefenstette25=(200,1000,0.01,'grefenstette25.txt')
+grefenstette26=(300,1000,0.01,'grefenstette26.txt')
+
+grefenstette30=(30,1000,0.0,'grefenstette30.txt')          #Pm
+grefenstette31=(30,1000,0.001,'grefenstette31.txt')   
+grefenstette32=(30,1000,0.05,'grefenstette32.txt')
+grefenstette33=(30,1000,0.1,'grefenstette33.txt')
+grefenstette34=(30,1000,0.2,'grefenstette34.txt')
+grefenstette35=(30,1000,0.4,'grefenstette35.txt')
+grefenstette36=(30,1000,0.6,'grefenstette36.txt')
+grefenstette37=(30,1000,0.9,'grefenstette37.txt')
+
 microga1=(5,100,0.04,'microga1.txt')
 microga2=(5,100,0.02,'microga2.txt')
 
-paramsGA=[dejong]
-#paramsGA=[dejong11,dejong12]
-#paramsGA=[dejong21,dejong22,dejong23,dejong24]
-#paramsGA=[dejong30,dejong31,dejong32,dejong33,dejong34,dejong35,dejong36,dejong37]
+bestparams01=(300,8000,0.01,'bestparams01.txt')
+bestparams02=(300,8000,0.60,'bestparams02.txt')
+
+#for dolphins
+paramsGA=[grefenstette,grefenstettedyn,grefenstette13,grefenstette26,grefenstette33,grefenstette36,bestparams01,bestparams02]
+
+#paramsGA=[bestparams01,bestparams02]
+#paramsGA=[grefenstette]
+#paramsGA=[grefenstettedyn]
+#paramsGA=[grefenstette11,grefenstette12,grefenstette13]
+#paramsGA=[grefenstette14,grefenstette15,grefenstette16,grefenstette17]
+#paramsGA=[grefenstette20,grefenstette21,grefenstette22,grefenstette23,grefenstette24,grefenstette25,grefenstette26]
+#paramsGA=[grefenstette30,grefenstette31,grefenstette32,grefenstette33,grefenstette34,grefenstette35,grefenstette36,grefenstette37]
 #paramsGA=[default,dejong,grefenstette,microga1,microga2]
 
 def calc_graph_weight(graph):
@@ -371,20 +396,16 @@ def improve_offspring(graph, offspring):
                             max_cluster=c
             # change to this c-luster
             if (max_intersect>-1):
-                global mutations
-                mutations+=1
-                old_mod=offspring[2]
-                candidate=tuple(dict(offspring[0]),dict(offspring[1]))
-                offspring[1][max_cluster].append(e)
-                del offspring[1][cluster_id]
-                offspring[0][e]=max_cluster
-                offspring=modularity(graph,offspring[0],offspring[1])
-                if (old_mod>offspring[2]):
-                    global error
-                    error+=1
-                else:
-                    global ok
-                    ok+=1
+                candidate=(copy.deepcopy(offspring[0]),copy.deepcopy(offspring[1]))
+                candidate[1][max_cluster].append(e)
+                del candidate[1][cluster_id]
+                candidate[0][e]=max_cluster
+                candidate=modularity(graph,candidate[0],candidate[1])
+                if (False or candidate[2]>offspring[2]):
+                    offspring=candidate
+                    global mutations
+                    mutations+=1
+    return offspring
                      
 
 def update_best(solution):
@@ -394,6 +415,9 @@ def update_best(solution):
         best_q=q
         global best
         best=solution
+        if (dyn):
+            global nochange
+            nochange=0
 
 def update_population3(graph, population, offspring):
     if offspring not in population:
@@ -428,7 +452,12 @@ def update_population1(graph, population, offspring):
 def end_criterion():
     global generations
     generations+=1
-    return generations>MAX_GENERATIONS
+    global nochange
+    nochange+=1
+    if (dyn):
+        return nochange>MAX_GENERATIONS
+    else:
+        return generations>MAX_GENERATIONS
 
 def calculate_total_q(population):
     total_q=0.0
@@ -443,7 +472,7 @@ def run(graph):
         (p1,p2)=choose_parents(population, total_q)
         offspring=combine_parents(graph,p1,p2)
         offspring=modularity(graph,offspring[0],offspring[1])  # remove later
-        improve_offspring(graph,offspring)
+        offspring=improve_offspring(graph,offspring)
         offspring=modularity(graph,offspring[0],offspring[1])
         update_best(offspring)    
         total_q=update_population(graph,population,offspring)
@@ -490,14 +519,9 @@ def results(graph):
     d_best=ERI_member(graph, best)
     d_BKR=ERI_member(graph, (rereverse(gt),gt))
     eri=ERI(d_best,d_BKR)
-    # eri dgt, dgt3 = 11/159 = 0.06918
-    # eri dgt, dgt2 = 0.2012
-    #d_BKR2=ERI_member(graph, (rereverse(dgt2), dgt2))
-    #eri2=ERI(d_BKR,d_BKR2)
-    results='BESTMOD;{0};;ERI;{1};;MUTATIONS;{2};CREPL;{3};WREPL;{4}'.format(best_q,eri,mutations,creplacements,wreplacements)
+    results='BESTMOD;{0};;GENS:{5};;;ERI;{1};;MUTATIONS;{2};CREPL;{3};WREPL;{4}'.format(best_q,eri,mutations,creplacements,wreplacements,generations)
     global results_num
     show(results)
-    print error, ok
     results_best_q.append(best_q)
     results_eri.append(eri)
 
@@ -539,10 +563,6 @@ def init_paramsGA(paramsGA):
     show(params)
 
 def init_exec():
-    global error
-    error=0
-    global ok
-    ok=0
     global generations
     generations=0
     global mutations
@@ -581,7 +601,8 @@ def problem(problem_name):
             init_exec()
             run(graph)
             results(graph)
-        closefile()   
+        closefile()
+        print generations
 
 def update_population(graph,population, offspring):
     update_population1(graph,population, offspring)
@@ -589,8 +610,8 @@ def update_population(graph,population, offspring):
     return total_q
 
 def main():
-    problem(karate_club)
-    #problem(dolphins)         
+    #problem(karate_club)
+    problem(dolphins)         
     
 
 # This is the standard boilerplate that calls the main() function.
