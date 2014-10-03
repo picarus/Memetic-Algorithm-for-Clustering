1ee5be8548ecbb12e37af451e3d5a66eabd2f0d6
Jose Antonio Magaña Mesa <jose_a_magana@hotmail.com>
2014-10-03 22:04:56 +1000

    remove private files

--- src - Copy/dolp.py
+++
@@ -1,8 +0,0 @@
-import cimemetics
-
-
-if __name__ == '__main__':
-    graph=cimemetics.loadData(cimemetics.dolphins)
-    print cimemetics.NMI(graph,(None,cimemetics.dgt),(None,cimemetics.dgt2))
-        
-        
